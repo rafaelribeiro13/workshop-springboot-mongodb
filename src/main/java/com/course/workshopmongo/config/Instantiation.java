428dec335d7bb8f2c52c2a01b895673909b08e1b
Rafael Ribeiro <rafaelsribeiro45@gmail.com>
2023-01-03 20:49:27 -0300

    Referencing user posts

--- src/main/java/com/course/workshopmongo/config/Instantiation.java
+++ src/main/java/com/course/workshopmongo/config/Instantiation.java
@@ -41,6 +41,9 @@ public class Instantiation implements CommandLineRunner {
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
+		u1.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(u1);
+		
 	}
 
 }
